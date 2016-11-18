7110a738a4f87f33dbea8b8cb2cb765b6271e59c
Hugo Kamps <hugokamps6@hotmail.com>
2016-11-18 15:54:18 +0100

    Extra tweaks LocationManager
    lat en lng attributen in klasse
    op elke locatie automatisch 2 markers

--- KBS SE3/KBS SE3/Core/Locationmanager.cs
+++ KBS SE3/KBS SE3/Core/Locationmanager.cs
@@ -8,6 +8,8 @@ namespace KBS_SE3.Core
     internal class LocationManager
     {
         private PictureBox _pictureBox;
+        private double _currentLongitude;
+        private double _currentLatitude;
         private readonly List<string> _points = new List<string>() {
             "52.501356,6.083451",
             "52.501988,6.082142"
@@ -21,11 +23,15 @@ namespace KBS_SE3.Core
         }
 
         private void watcher_PositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e) {
-            var coordinates = e.Position.Location.Latitude + "," + e.Position.Location.Longitude;
-            _pictureBox.Load(GetMap(coordinates, _points));
+            _currentLatitude = e.Position.Location.Latitude;
+            _currentLongitude = e.Position.Location.Longitude;
+            _points[0] = (_currentLatitude + 0.005) + ", " + (_currentLongitude + 0.005);
+            _points[1] = (_currentLatitude - 0.005) + ", " + (_currentLongitude - 0.005);
+            _pictureBox.Load(GetMap(_currentLatitude, _currentLongitude, _points));
         }
 
-        public string GetMap(string currentLocation, List<string> locations) {
+        public string GetMap(double currentLatitude, double currentLongitude, List<string> locations) {
+            var currentLocation = currentLatitude + ", " + currentLongitude;
             var url = "https://maps.googleapis.com/maps/api/staticmap?center=" + currentLocation +
                       "&zoom=15&size=700x480&maptype=terrain&";
             url += "markers=color:blue%7Clabel:L%7C" + currentLocation + "&";
