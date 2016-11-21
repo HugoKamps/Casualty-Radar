4cf3402aaa775a73d2bc3de013f584fc9a0ca3a2
Dennisrump <dennisrump10@hotmail.com>
2016-11-21 13:24:53 +0100

    Formatting

--- KBS SE3/KBS SE3/Models/FeedTicker.cs
+++ KBS SE3/KBS SE3/Models/FeedTicker.cs
@@ -15,15 +15,13 @@ namespace KBS_SE3.Models
         private static Feed _feed;
         private Timer _stateTimer = new Timer();
 
-        public FeedTicker(int t, Feed f)
-        {
+        public FeedTicker(int t, Feed f) { 
             _tickTime = t;
             _feed = f;
             startTimer();
         }
 
-        public static void Tick(Object sender, EventArgs e)
-        {
+        public static void Tick(Object sender, EventArgs e) { 
             _feed.UpdateFeed();
         }
 
@@ -32,8 +30,7 @@ namespace KBS_SE3.Models
         * Starts the timer with a standard timetick interval (set to 30 seconds as standard)
         * Adds an eventhandler to the feeds get updated everytime stateTimer ticks
         */
-        private void startTimer()
-        {
+        private void startTimer() {
             _stateTimer.Interval = (_tickTime);
             _stateTimer.Tick += new EventHandler(Tick);
             _stateTimer.Start();
