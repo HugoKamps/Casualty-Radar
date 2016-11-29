68634743ba56ba2500ad76cb8ae1ec6bd5ecb766
MaartenBrandemann <maartenbrandemann@live.nl>
2016-11-29 10:10:48 +0100

    Added normal graph classes

---
+++ KBS SE3/KBS SE3/Models/Graph/Edge.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KBS_SE3.Models.Graph {
+     class Edge {
+        public Vertex dest;   /* Second 				vertex in Edge */
+        public double cost;   // Edge cost        
+
+        public Edge(Vertex dest, double cost) {
+            this.dest = dest;
+            this.cost = cost;
+
+        }
+    }
+}
