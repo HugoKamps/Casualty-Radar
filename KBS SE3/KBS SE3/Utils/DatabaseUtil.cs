c0d28cb5fcbf6cdcf3018dad1dad82e4541aba05
Dennisrump <dennisrump10@hotmail.com>
2016-12-01 13:39:09 +0100

    Formatting

--- KBS SE3/KBS SE3/Utils/DatabaseUtil.cs
+++ KBS SE3/KBS SE3/Utils/DatabaseUtil.cs
@@ -10,48 +10,47 @@ namespace KBS_SE3.Utils
 {
     class DatabaseUtil
     {
-        
-            private static DatabaseUtil _dbInstance;
+        private static DatabaseUtil _dbInstance;
         MySqlConnection connection = new MySqlConnection(
            "Server=studiomajestic.nl;user id=studiop52_radar;password=koffie;database=studiop52_radar;");
 
-            public static DatabaseUtil getDbInstance()
+        public static DatabaseUtil getDbInstance()
+        {
+            if (_dbInstance == null)
             {
-                if (_dbInstance == null)
-                {
                 _dbInstance = new DatabaseUtil();
-                }
-                return _dbInstance;
             }
-
-            public void GetDBConnection()
-            {
-                try
-                {
-                    connection.Open();
-                    Console.WriteLine("Connected");
-                }
-                catch (SqlException e)
-                {
-                    Console.WriteLine("Not connected : " + e.ToString());
-                }
-            }
-
-            public void selectAllNodeId()
-            {
-                lock (connection)
-                {
-                    using (var command = new MySqlCommand("SELECT id FROM node", connection))
-                    {
-                        command.Prepare();
-                        using (var reader = command.ExecuteReader())
-                            while (reader.Read())
-                            {
-                                Console.WriteLine("id: " + reader.GetInt32(0));
-                            }
-                    }
-                }
-            }
-
+            return _dbInstance;
         }
+
+        public void GetDBConnection()
+        {
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Connected");
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Not connected : " + e.ToString());
+            }
+        }
+
+        public void selectAllNodeId()
+        {
+            lock (connection)
+            {
+                using (var command = new MySqlCommand("SELECT id FROM node", connection))
+                {
+                    command.Prepare();
+                    using (var reader = command.ExecuteReader())
+                        while (reader.Read())
+                        {
+                            Console.WriteLine("id: " + reader.GetInt32(0));
+                        }
+                }
+            }
+        }
+
     }
+}
