78159f604f0835dbe49b812ac36848df05fa915b
MaartenBrandemann <maartenbrandemann@live.nl>
2016-12-15 12:43:01 +0100

    ja

--- KBS SE3/KBS SE3/Modules/NavigationModule.cs
+++ KBS SE3/KBS SE3/Modules/NavigationModule.cs
@@ -75,7 +75,7 @@ namespace KBS_SE3.Modules {
             parser.Deserialize();
             DataCollection collection = parser.GetCollection();
             //_locationManager.DrawRoute(collection, _routeOverlay);
-            _locationManager.drawTestRoute(collection, _routeOverlay);
+            //_locationManager.drawTestRoute(collection, _routeOverlay);
         }
 
         public void CreateRouteStepPanel(NavigationStep step, Color color, int y) {
