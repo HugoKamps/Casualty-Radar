5dc8577fdd9f50aed99b3fdcc1cf667c7e959d5f
dubehh <eelcoeikelboom@hotmail.com>
2016-12-20 13:48:22 +0100

    Renamed file + prevented keyNotFoundExc.

--- KBS SE3/KBS SE3/Models/DataControl/Graph/Ways/WayTypeControl.cs
+++ KBS SE3/KBS SE3/Models/DataControl/Graph/Ways/WayTypeControl.cs
@@ -34,7 +34,10 @@ namespace KBS_SE3.Models.DataControl.Graph.Ways {
         * This method is used to link ways to the proper WayTypeBase using a dictionary.
         * Dictionaries are much faster than a list in this case because we don't require a loop.
         */
-        public WayTypeBase GeTypeBase(string key) => _typeMap[key];
+        public WayTypeBase GeTypeBase(string key) {
+            WayTypeBase rtn = null;
+            return _typeMap.TryGetValue(key, out rtn) ? rtn : null;
+        } 
 
         // TODO
         public List<Way> GetByZoomLevel() {
