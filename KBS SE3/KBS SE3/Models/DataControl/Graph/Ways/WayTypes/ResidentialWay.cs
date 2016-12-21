e39fb26003665502de049625e94ead488d05c9cb
Dubehh <eelcoeikelboom@hotmail.com>
2016-12-21 16:17:49 +0100

    Residential Way

---
+++ KBS SE3/KBS SE3/Models/DataControl/Graph/Ways/WayTypes/ResidentialWay.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casualty_Radar.Models.DataControl.Graph.Ways.WayTypes {
+    public class ResidentialWay : WayTypeBase {
+
+        public ResidentialWay() : base("res", "Residential", WayZoomLevel.Regional) { }
+
+    }
+}
+    
