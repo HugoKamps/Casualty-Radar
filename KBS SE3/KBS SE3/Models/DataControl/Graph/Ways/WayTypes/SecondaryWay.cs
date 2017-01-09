98d261e67476c33b483ed818e1dd1ff5d51aa496
Hugo Kamps <hugokamps6@hotmail.com>
2017-01-09 12:46:12 +0100

    WayTypes toegevoegd

---
+++ KBS SE3/KBS SE3/Models/DataControl/Graph/Ways/WayTypes/SecondaryWay.cs
@@ -0,0 +1,5 @@
+﻿namespace Casualty_Radar.Models.DataControl.Graph.Ways.WayTypes {
+    class SecondaryWay : WayTypeBase {
+        public SecondaryWay() : base("sec", "Secondary", WayZoomLevel.Regional) { }
+    }
+}
