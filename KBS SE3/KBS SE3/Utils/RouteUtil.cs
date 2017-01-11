dd5b818c24157fba4d83b58bf79eadd8a3ea97e2
Hugo Kamps <hugokamps6@hotmail.com>
2017-01-11 10:02:06 +0100

    Comments toegevoegd

--- KBS SE3/KBS SE3/Utils/RouteUtil.cs
+++ KBS SE3/KBS SE3/Utils/RouteUtil.cs
@@ -2,9 +2,16 @@
 using Casualty_Radar.Models.DataControl.Graph;
 
 namespace Casualty_Radar.Utils {
-    class RouteUtil {
+    class RouteUtil
+    {
+         /// <summary>		
+         /// Get the angle in degrees between two lat & long points		
+         /// </summary>		
+         /// <param name="node1">The current node in the route</param>		
+         /// <param name="node2">The next node in the route</param>				
+         /// <returns>The angle between two coordinates</returns>
         public static double AngleFromCoordinate(Node node1, Node node2) {
-            double dLon = (node2.Lon - node1.Lon);
+            double dLon = node2.Lon - node1.Lon;
 
             double y = Math.Sin(dLon) * Math.Cos(node2.Lat);
             double x = Math.Cos(node1.Lat) * Math.Sin(node2.Lat) - Math.Sin(node1.Lat)
@@ -19,6 +26,13 @@ namespace Casualty_Radar.Utils {
             return brng;
         }
 
+        /// <summary>
+        /// Calculate the bearing between two angles
+        /// If it is lower than 0, add 360 to it so it will always be a positive number
+        /// </summary>
+        /// <param name="angle1">The angle of the current node</param>
+        /// <param name="angle2">The angle of the next node</param>
+        /// <returns>The bearing between two angles</returns>
         public static double CalcBearing(double angle1, double angle2) {
             double bearing = angle2 - angle1;
 
