9de7ff6629993997445af30e9a7d07f14cc68664
Hugo Kamps <hugokamps6@hotmail.com>
2017-01-16 18:53:11 +0100

    Comments

--- KBS SE3/KBS SE3/Models/GeoMapSection.cs
+++ KBS SE3/KBS SE3/Models/GeoMapSection.cs
@@ -2,11 +2,14 @@
 
 namespace Casualty_Radar.Models
 {
+    /// <summary>
+    /// Class that contains the boundary data and filename of each XML file
+    /// </summary>
     class GeoMapSection
     {
-        public PointLatLng UpperBound { get; set; }
-        public PointLatLng LowerBound { get; set; }
-        public string TargetFilePath { get; set; }
+        public PointLatLng UpperBound { get; set; } // The coordinates for the upper boundary
+        public PointLatLng LowerBound { get; set; } // The coordinates for the lower boundary
+        public string TargetFilePath { get; set; }  // The filename of the XML file
 
         public GeoMapSection(PointLatLng upperBound, PointLatLng lowerBound, string targetFilePath) {
             UpperBound = upperBound;
