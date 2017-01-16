c14fcce0f036c55076db009370612e7e25a2cba4
Richard96 <richard10@live.nl>
2017-01-16 12:05:18 +0100

    Testmodule aangemaakt

---
+++ KBS SE3/KBS SE3/Modules/TestModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Casualty_Radar.Core;
+using Casualty_Radar.Models;
+
+namespace Casualty_Radar.Modules
+{
+    partial class TestModule : UserControl, IModule
+    {
+        public TestModule()
+        {
+            InitializeComponent();
+        }
+
+        public Breadcrumb GetBreadcrumb()
+        {
+            return new Breadcrumb(this, "Testmodule");
+        }
+    }
+}
