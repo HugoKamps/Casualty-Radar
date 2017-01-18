6b28a4a14e20c3a895431af2710b3e008820075d
Dennis Rump <Dennisrump10@hotmail.com>
2017-01-18 12:18:11 +0100

    App icon swag lol yolo

--- KBS SE3/KBS SE3/Properties/Resources.Designer.cs
+++ KBS SE3/KBS SE3/Properties/Resources.Designer.cs
@@ -61,6 +61,16 @@ namespace Casualty_Radar.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon app_icon {
+            get {
+                object obj = ResourceManager.GetObject("app_icon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap destination_icon {
