91df82d1596699df09817089fca75196fe930747
Dubehh <eelcoeikelboom@hotmail.com>
2017-01-20 14:12:40 +0100

    Replaced all abstract waytypebases with enum constants

---
+++ KBS SE3/KBS SE3/Models/DataControl/Graph/Ways/WayType.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casualty_Radar.Models.DataControl.Graph.Ways {
+
+
+    /// <summary>
+    /// A WayType is a simple description of a way.
+    /// Each waytype is visible in a specific zoomlevel, this
+    /// can be used to draw only specific ways to increase loadingspeed.
+    /// </summary>
+    public enum WayType {
+
+        LivingStreet = WayZoomLevel.Local,
+        MotorWay = WayZoomLevel.National,
+        Path = WayZoomLevel.Local,
+        MotorWayLink = WayZoomLevel.National,
+        PrimaryLink = WayZoomLevel.National,
+        PrimaryWay = WayZoomLevel.National,
+        ResidentialWay = WayZoomLevel.Local,
+        SecondaryLink = WayZoomLevel.Regional,
+        SecondaryWay = WayZoomLevel.Regional,
+        TertiaryLink = WayZoomLevel.Regional,
+        TertiaryWay = WayZoomLevel.Regional,
+        Trunk = WayZoomLevel.National,
+        TrunkLink = WayZoomLevel.National,
+        UnclassifiedWay = WayZoomLevel.Local
+
+    }
+}
